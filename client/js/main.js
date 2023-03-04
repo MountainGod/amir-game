eb1fbb47a59f255689aa51199150f4954423a358
Harel Karni <MountainGodRays@gmail.com>
2023-03-04 17:07:17 +0200

    code clean up

--- client/js/main.js
+++ client/js/main.js
@@ -1,129 +1,120 @@
+const redPlayer    = "R";
+const yellowPlayer = "Y";
+let currentPlayer  = redPlayer;
 
-const RedPlayer      ="R";
-const YellowPlayer = "Y";
-let CurrentPlayer = RedPlayer;
-let CurrentCols;
-let GameOver=false;
+let currentCols;
+let gameOver = false;
+
 let board;
-const rows=6;
-const cols=7;
-let button=document.getElementById("button");
+const ROWS = 6;
+const COLS = 7;
 
-window.onload=function(){
-	setGame();
-}
-function setGame(){
-	board=[];
-	CurrentCols=[5, 5, 5, 5, 5, 5, 5];
+window.onload = setGame;
+
+function setGame() {
+	board       = [];
+	currentCols = [5, 5, 5, 5, 5, 5, 5];
 	
-	for (let i = 0; i <rows ; i++) {
-		let row=[];
-		for (let j = 0; j < cols; j++) {
-			row.push(' ');
-			let piece=document.createElement("div");
-			piece.id = i.toString() + "-" +j.toString();
+	for (let i = 0; i < ROWS; i++) {
+		let row = [];
+		for (let j = 0; j < COLS; j++) {
+			row.push(" ");
+			let piece = document.createElement("div");
+			piece.id  = i.toString() + "-" + j.toString();
 			piece.classList.add("piece");
-			piece.addEventListener("click",setPiece);
+			piece.addEventListener("click", setPiece);
 			document.getElementById("board").append(piece);
 			
 			
 		}
 		board.push(row);
-		
 	}
-	
 }
-function setPiece(){
-	
-	if(GameOver){
+
+function setPiece() {
+	if (gameOver) {
 		return;
 	}
-	let position=this.id.split("-");
-	let r = parseInt(position[0]);
-	let c = parseInt(position[1]);
-	r=CurrentCols[c];
-	if(r<0){
+	let position = this.id.split("-");
+	let col      = parseInt(position[1]);
+	let row      = currentCols[col];
+	if (row < 0) {
 		return;
 	}
-	board[r][c]=CurrentPlayer;
-	let piece=document.getElementById(r.toString()+"-"+c.toString());
-	if(CurrentPlayer==RedPlayer){
-		piece.classList.add("red")
-		CurrentPlayer=YellowPlayer;
+	board[row][col] = currentPlayer;
+	let piece       = document.getElementById(row.toString() + "-" + col.toString());
+	if (currentPlayer === redPlayer) {
+		piece.classList.add("red");
+		currentPlayer = yellowPlayer;
 	}
-	else{
-		piece.classList.add("yellow")
-		CurrentPlayer=RedPlayer;
+	else {
+		piece.classList.add("yellow");
+		currentPlayer = redPlayer;
 	}
-	r-=1;
-	CurrentCols[c]=r;
+	row -= 1;
+	currentCols[col] = row;
 	
 	checkWinner();
-	
 }
 
-function checkWinner(){
-	for (let i = 0; i < rows; i++) {
-		for (let j = 0; j < cols-3; j++) {
-			if (board[i][j]!=" ") {
-				if (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] && board[i][j] == board[i][j + 3]) {
+function checkWinner() {
+	for (let i = 0; i < ROWS; i++) {
+		for (let j = 0; j < COLS - 3; j++) {
+			if (board[i][j] !== " ") {
+				if (board[i][j] === board[i][j + 1] && board[i][j] === board[i][j + 2] && board[i][j] === board[i][j + 3]) {
 					setWinner(i, j);
 					return;
 				}
 			}
 		}
-		
-	}
-	for (let j = 0; j < cols; j++) {
-		for (let i = 0; i < rows-3; i++) {
-			if (board[i][j]!=" ") {
-				if (board[i][j] == board[i+1][j] && board[i][j] == board[i+2][j] && board[i][j] == board[i+3][j]) {
-					setWinner(i, j);
-					return;
-				}
-			}
-		}
-		
 	}
 	
-	for (let i = 0; i < rows-3; i++) {
-		for (let j = 0; j < cols-3; j++) {
-			if (board[i][j]!=" ") {
-				if (board[i][j] == board[i+1][j+1] && board[i][j] == board[i+2][j+2] && board[i][j] == board[i+3][j+3]) {
+	for (let j = 0; j < COLS; j++) {
+		for (let i = 0; i < ROWS - 3; i++) {
+			if (board[i][j] !== " ") {
+				if (board[i][j] === board[i + 1][j] && board[i][j] === board[i + 2][j] && board[i][j] === board[i + 3][j]) {
 					setWinner(i, j);
 					return;
 				}
 			}
 		}
-		
 	}
-	for (let i = 3; i < rows; i++) {
-		for (let j = 0; j < cols-3; j++) {
-			if (board[i][j]!=" ") {
-				if (board[i][j] == board[i-1][j+1] && board[i][j] == board[i-2][j+2] && board[i][j] == board[i-3][j+3]) {
+	
+	for (let i = 0; i < ROWS - 3; i++) {
+		for (let j = 0; j < COLS - 3; j++) {
+			if (board[i][j] !== " ") {
+				if (board[i][j] === board[i + 1][j + 1] && board[i][j] === board[i + 2][j + 2] && board[i][j] === board[i + 3][j + 3]) {
 					setWinner(i, j);
 					return;
 				}
 			}
 		}
-		
 	}
 	
-	
-}
-function setWinner(i,j){
-	let winner=document.getElementById("winner");
-	if(board[i][j]==RedPlayer){
-		winner.innerText="Red Wins";
+	for (let i = 3; i < ROWS; i++) {
+		for (let j = 0; j < COLS - 3; j++) {
+			if (board[i][j] !== " ") {
+				if (board[i][j] === board[i - 1][j + 1] && board[i][j] === board[i - 2][j + 2] && board[i][j] === board[i - 3][j + 3]) {
+					setWinner(i, j);
+					return;
+				}
+			}
+		}
 	}
-	else{
-		winner.innerText="Yellow Wins";
-	}
-	GameOver=true;
-	return;
 }
 
-function resetGame(){
-	GameOver=false;
+function setWinner(i, j) {
+	let winner = document.getElementById("winner");
+	if (board[i][j] === redPlayer) {
+		winner.innerText = "Red Wins";
+	}
+	else {
+		winner.innerText = "Yellow Wins";
+	}
+	gameOver = true;
+}
+
+function resetGame() {
+	gameOver = false;
 	setGame();
 }
